439c3db2fd7e177e1dea1cc2324f529f69ef35f3
Brauperle <lilian.tourillon@gmail.com>
2021-02-24 00:19:05 +0100

    trying to resolve vercel build issue

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
 import App from './app/App'
-import { StoreContextProvider } from './store/StoreProvider'
+// import { StoreContextProvider } from './store/StoreProvider'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <StoreContextProvider>
-      <App />
-    </StoreContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <StoreContextProvider>
+//       <App />
+//     </StoreContextProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
+console.log("INDEXJSX")
+console.log(App);
