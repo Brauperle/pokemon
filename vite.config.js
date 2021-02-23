2b8de439ffb1f31f089342c1a882ae42d9be7cfa
Brauperle <lilian.tourillon@gmail.com>
2021-02-23 21:50:52 +0100

    trying to resolve vercel build issue

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,30 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
+// import { nodeResolve } from '@rollup/plugin-node-resolve'
+// import {terser} from 'rollup-plugin-terser';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root:"./",
   plugins: [
     reactRefresh()
   ],
   build: {
-    rollupOptions: {
-      plugins: [
-        nodeResolve({
-          extensions: ['.js', '.jsx']
-        })
-      ]
-    }
+    // minify:"terser",
+    // terserOptions: {
+
+    // },
+    // cleanCssOptions: {
+
+    // },
+    // rollupOptions: {
+    //   plugins: [
+    //     // terser({
+    //     // })
+    //     // nodeResolve({
+    //     //   extensions: ['.js', '.jsx']
+    //     // })
+    //   ]
+    // }
   }
 })
