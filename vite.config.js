5a093899ea25cd181f5d1fb5ce8dcc2f66fa1af7
Brauperle <lilian.tourillon@gmail.com>
2021-02-24 00:52:30 +0100

    trying to debug vercel build (#5)
    * trying to debug vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+// import commonjs from '@rollup/plugin-commonjs'
+// import babel from '@rollup/plugin-babel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  build: {
-    rollupOptions: {}
-  }
+  plugins: [
+    reactRefresh()
+    // {
+    //   ...commonjs(), // CommonJS modules to ES6
+    //   apply: 'build' // Only used when using vite build
+    // },
+    // {
+    //   ...babel(), // integration between Rollup and Babel
+    //   apply: 'build' // Only used when using vite build
+    // }
+  ]
 })
